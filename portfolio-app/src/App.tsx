6ae8069715d8332db74ad3457ae5bb608931ba8f
rob-villarre <roberto@dynamicvision.ca>
2022-08-31 16:36:51 -0600

    added JetBrainsMono font

--- portfolio-app/src/App.tsx
+++ portfolio-app/src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
+import './fonts.css';
+
 import Navbar from './components/navbar'
 
 function App() {
   return (
-    <div className="App d-flex flex-column min-vh-100">
+    <div className="App default-font d-flex flex-column min-vh-100">
       <header className="App-header">
         <Navbar />
       </header>
-      <body>
-      
-      </body>
+      <strong>dsad </strong>hello hopw asd adas da d0o
       <footer>
 
       </footer>
--- portfolio-app/src/index.tsx
+++ portfolio-app/src/index.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/js/bootstrap';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement

e58a6a8a2159942726bf1635639a6e36214fde68
rob-villarre <roberto@dynamicvision.ca>
2022-12-05 17:48:34 -0700

    added terraform for hosting in digitalocean

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+resource "digitalocean_project" "portfolio" {
+  name        = "Portfolio"
+  purpose = "Website or blog"
+  description = "My portfolio project"
+  environment = "Development"
+  is_default  = true  
+
+  resources   = [ digitalocean_app.portfolio-app.urn ]
+}
+
+resource "digitalocean_app" "portfolio-app" {
+
+  spec {
+    name    = "portfolio-app"
+    region  = "tor"
+
+    static_site {
+      build_command     = "npm run build"
+      environment_slug  = "node-js"
+      name              = "portfolio-website"
+      source_dir        = "portfolio-app"
+
+      github {
+        branch          = "main"
+        deploy_on_push  = true
+        repo            = "rob-villarre/portfolio-website"
+      }
+
+      routes {
+        path = "/"
+        preserve_path_prefix = false
+      }
+    }
+  }
+
+}

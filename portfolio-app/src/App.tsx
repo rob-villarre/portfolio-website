c30c76800f26c5ba8427fcfb179a4d33bc6f2701
rob-villarre <roberto@dynamicvision.ca>
2022-12-07 17:53:57 -0700

    added scroll animation

--- portfolio-app/src/App.tsx
+++ portfolio-app/src/App.tsx
@@ -1,10 +1,60 @@
 import React from 'react';
+import { useEffect } from 'react';
+
 import './css/fonts.css';
 import './css/index.scss';
+
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 
 import Navbar from './components/navbar'
 // flex-grow-1
 function App() {
+  useEffect(() => {
+    gsap.set('.section', {autoAlpha: 0});
+    gsap.utils.toArray<HTMLElement>('.section').forEach((elem) => {
+      gsap.registerPlugin(ScrollTrigger);
+      ScrollTrigger.create({
+        trigger: elem,
+        start: "top 80%",
+        end: "bottom 20%",
+        markers: false,
+        onEnter: function () {
+          gsap.fromTo(
+            elem,
+            { y: 100, autoAlpha: 0 },
+            {
+              duration: 1.25,
+              y: 0,
+              autoAlpha: 1,
+              ease: "back",
+              overwrite: "auto"
+            }
+          );
+        },
+        onLeave: function () {
+          gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+        },
+        onEnterBack: function () {
+          gsap.fromTo(
+            elem,
+            { y: -100, autoAlpha: 0 },
+            {
+              duration: 1.25,
+              y: 0,
+              autoAlpha: 1,
+              ease: "back",
+              overwrite: "auto"
+            }
+          );
+        },
+        onLeaveBack: function () {
+          gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+        }
+      });
+    });
+
+  }, [])
   return (
     <div className=''>
       <header className="App-header">
@@ -13,8 +63,8 @@ function App() {
       <body className=''>
 
         {/* Hero */}
-        <div className=' min-vh-100 px-4 py-5' id='hero'>
-          <div className='py-5'>
+        <div className='min-vh-100 px-4 py-5' id='hero'>
+          <div className='section py-5'>
             <h1 className='display-5 fw-bold mx-5'>Hello World!</h1>
             <div className='col-lg-6 mx-5'>
               <p className='fs-5 mb-4'>My name is Roberto Villarreal Andrade</p>
@@ -26,11 +76,10 @@ function App() {
           </div>
         </div>
         {/* About */}
-        <div className=' min-vh-100 px-4 py-5 bg-light' id='hero'>
-          <div className='py-5'>
-            <h1 className='display-5 fw-bold mx-5'>Hello World!</h1>
+        <div className='min-vh-100 px-4 py-5' id='hero'>
+          <div className='section py-5'>
+            <h1 className='display-5 fw-bold mx-5'>About Me</h1>
             <div className='col-lg-6 mx-5'>
-              <p className='fs-5 mb-4'>My name is Roberto Villarreal Andrade</p>
               <p className='text-white-50'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -40,11 +89,10 @@ function App() {
         </div>
 
         {/* About */}
-        <div className=' min-vh-100 px-4 py-5 bg-info' id='hero'>
-          <div className='py-5'>
-            <h1 className='display-5 fw-bold mx-5'>Hello World!</h1>
+        <div className='min-vh-100 px-4 py-5' id='hero'>
+          <div className='section py-5'>
+            <h1 className='display-5 fw-bold mx-5'>Another Section</h1>
             <div className='col-lg-6 mx-5'>
-              <p className='fs-5 mb-4'>My name is Roberto Villarreal Andrade</p>
               <p className='text-white-50'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
